83e2cd97500b12b5a63a3ffc9ac56dca66467d1e
Anderson_Paulino <xandeerson@gmail.com>
2023-08-15 19:45:12 -0300

    Primeiro Commit do projeto Pizzaria-api.

---
+++ app/src/main/java/com/pizzaria/app/entity/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.pizzaria.app.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AbstractEntity {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false, unique = true)
+    private Long id;
+    @Getter @Setter
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+    @Getter @Setter
+    @Column(name = "registro", nullable = false)
+    private LocalDateTime registro;
+    @Getter @Setter
+    @Column(name = "atualizar")
+    private LocalDateTime atualizar;
+
+    @PrePersist
+    private void prePersist(){
+        this.registro = LocalDateTime.now();
+        this.ativo = true;
+    }
+    @PreUpdate
+    private void preUpdate(){
+        this.atualizar = LocalDateTime.now();
+    }
+}
