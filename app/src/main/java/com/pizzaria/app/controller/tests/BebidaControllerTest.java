c03b5038caeb71d98bc44f658f463be005e22e74
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-09-20 21:44:11 -0300

    Commit de metodos de listar.

--- app/src/main/java/com/pizzaria/app/controller/tests/BebidaControllerTest.java
+++ app/src/main/java/com/pizzaria/app/controller/tests/BebidaControllerTest.java
@@ -51,11 +51,15 @@ public class BebidaControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(bebidaController).build();
         objectMapper = new ObjectMapper(); // Initialize objectMapper here
         bebidaDTO.setNomeBebida("Coca-Cola");
+        bebidaDTO.setValorBebida(BigDecimal.valueOf(5.0));
+        bebidaDTO.setAtivo(true);
+        bebidaDTO.setRegistro(LocalDateTime.now());
+        bebidaDTO.setId(1L);
     }
 
     @Test
     public void testFindById() throws Exception {
-        BebidaDTO bebidaDTO = new BebidaDTO(1L, "Coca-Cola", BigDecimal.valueOf(5.0), true, LocalDateTime.now());
+
         when(bebidaService.findById(1L)).thenReturn(bebidaDTO);
 
         mockMvc.perform(get("/api/bebida/1")
