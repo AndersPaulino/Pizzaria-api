6ad610e5019039a59ae46a7f11eedb16513cad7a
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-10-18 23:38:39 -0300

    fixes

--- pizzaria-front/pizzaria-app/src/app/app-routing.module.ts
+++ pizzaria-front/pizzaria-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BebidasListComponent } from './components/bebida/bebidalist/bebidalist.component';
+import { LoginComponent } from './components/sistema/login/login.component';
+import { IndexComponent } from './components/layout/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: 'full' },
+  { path: "login", component: LoginComponent },
+  {
+    path: "admin", component: IndexComponent, children: [
+      { path: "bebidas", component: BebidasListComponent }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pizzaria-front/pizzaria-app/src/app/app.module.ts
+++ pizzaria-front/pizzaria-app/src/app/app.module.ts
@@ -1,21 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BebidasListComponent } from './components/bebida/bebidalist/bebidalist.component';
 import { BebidaDetailsComponent } from './components/bebida/bebidadetails/bebidadetails.component';
-
+import { LoginComponent } from './components/sistema/login/login.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { IndexComponent } from './components/layout/index/index.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     BebidasListComponent,
-    BebidaDetailsComponent
+    BebidaDetailsComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    IndexComponent
   ],
   imports: [
+    FormsModule,
     RouterModule,
     BrowserModule,
+    NgbModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
