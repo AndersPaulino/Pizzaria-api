e398d44407e69bceb3ca405edf73cc239b9731e4
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-11-20 22:40:07 -0300

    Security

---
+++ app/src/main/java/com/pizzaria/app/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.pizzaria.app.controller;
+
+import com.pizzaria.app.entity.UserEntity;
+import com.pizzaria.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserEntity> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public UserEntity createUser(@RequestBody UserEntity user) {
+        String encodedPassword = passwordEncoder().encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        return userService.createUser(user);
+    }
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ app/src/main/java/com/pizzaria/app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pizzaria.app.repository;
+
+import com.pizzaria.app.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
